c16c5ce994dd94d07c143aed4311a5317198040b
Maryiamreda <maryiamreda@gmail.com>
2025-05-16 17:39:49 +0300

    PageProps Type Errors in Next.js

--- src/app/admin/editevent/[eventId]/page.tsx
+++ src/app/admin/editevent/[eventId]/page.tsx
@@ -1,10 +1,10 @@
 import { getEventById } from '../../../../../backend/controllers/eventsController';
 import Form from '../Form';
 
-export default async function EditEvent({ params }: { params: { eventId: string } }) { 
-    const event = await getEventById(params.eventId);
-    
-    return (
-        <Form event={event} />
-    );
+export default async function EditEvent({ params }: { params: Promise<{ eventId: string }> }) {
+    const { eventId } = await params;
+
+    const event = await getEventById(eventId);
+
+    return <Form event={event} />;
 }
